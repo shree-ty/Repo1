7b0bb60b9639e841fa9e66ee42eb253002750b22
root <root@ip-172-31-17-100.ec2.internal>
2021-05-05 02:49:37 +0000

    commiting stash file

--- hello.java
+++ hello.java
@@ -2,7 +2,11 @@ class Hello
 {
 public static void main(String args[])
 {
+<<<<<<< Updated upstream
 System.out.println("hello from master");
+=======
+System.out.println("Making wrong changes branch");
+>>>>>>> Stashed changes
 }
 }
 
